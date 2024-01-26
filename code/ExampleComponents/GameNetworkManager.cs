246db7b70616ae20c10439c326137a0b57741ba6
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-01-25 20:50:18 -0500

    updated compontents from sbox-scenestaging

--- code/ExampleComponents/GameNetworkManager.cs
+++ code/ExampleComponents/GameNetworkManager.cs
@@ -1,30 +1,27 @@
-using Sandbox;
+namespace Sandbox;
 
 public sealed class GameNetworkManager : Component, Component.INetworkListener
 {
 	[Property] public GameObject PlayerPrefab { get; set; }
 	[Property] public GameObject SpawnPoint { get; set; }
 
-	protected override void OnStart()
-	{
-
-	}
-
-	protected override void OnUpdate()
-	{
-		
-	}
-
 	public void OnActive( Connection channel )
 	{
-		Log.Info( $"Player '{channel.DisplayName}' is becoming active" );
+		var clothing = new ClothingContainer();
+		clothing.Deserialize( channel.GetUserData( "avatar" ) );
 
-		var player = SceneUtility.Instantiate( PlayerPrefab, SpawnPoint.Transform.World );
+		var player = PlayerPrefab.Clone( SpawnPoint.Transform.World );
 
 		var nameTag = player.Components.Get<NameTagPanel>( FindMode.EverythingInSelfAndDescendants );
 		if ( nameTag is not null )
 		{
 			nameTag.Name = channel.DisplayName;
+		}
+
+		// Assume that if they have a skinned model renderer, it's the citizen's body
+		if ( player.Components.TryGet<SkinnedModelRenderer>( out var body, FindMode.EverythingInSelfAndDescendants ) )
+		{
+			clothing.Apply( body );
 		}
 
 		player.Network.Spawn( channel );
