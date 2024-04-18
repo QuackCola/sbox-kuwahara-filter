5c9e18e43a9534c6b1bf02aa925f8cd6e87ca6db
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-04-18 07:03:45 -0400

    new package structure

--- cleanup_compiled.cmd
+++ cleanup_compiled.cmd
@@ -2,7 +2,9 @@
 cls
 title Deleting Compiled Assets
 
+pushd "Assets"
 del /s *.vmdl_c *.vmat_c *.vtex_c *.generated.vtex_c *.vsnd_c *.sound_c *.vpcf_c *.vpost_c *.shader_c
+popd
 
 echo.
 echo [92m========== Finished! ==========[37m
